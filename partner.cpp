df3003dfc5365deacc9c82e159902c3586060111
xxxcc <xxxcc@outlook.com>
2023-06-18 20:46:36 +0800

    添加资源文件

--- partner.cpp
+++ partner.cpp
@@ -6,7 +6,7 @@ Partner::Partner(QWidget *par, quint32 p): QLabel(par), ip(p)
 {
     this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
     w = ((QWidget *)this->parent())->size().width();
-    this->setPixmap(QPixmap::fromImage(QImage(":/myImage/1.jpg").scaled(w - 10, w - 10)));
+    this->setPixmap(QPixmap::fromImage(QImage(":/myImage/resourse/1.jpg").scaled(w - 10, w - 10)));
     this->setFrameShape(QFrame::Box);
 
     this->setStyleSheet("border-width: 1px; border-style: solid; border-color:rgba(0, 0, 255, 0.7)");
