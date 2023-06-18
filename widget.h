17e42dba998d611fbfd2d080118c9ae5445b483d
xxxcc <xxxcc@outlook.com>
2023-06-18 23:47:42 +0800

    临时修改

--- widget.h
+++ widget.h
@@ -2,6 +2,16 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QCamera>
+#include <QImageCapture>
+#include <QMap>
+
+#include "sendimg.h"
+#include "recvsolve.h"
+#include "sendtext.h"
+#include "mytcpsocket.h"
+#include "audioinput.h"
+#include "audiooutput.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -17,5 +27,39 @@ public:
 
 private:
     Ui::Widget *ui;
+
+    static QRect pos;
+    quint32 mainIp; //主屏幕显示的IP图像
+    QCamera *_camera;
+    QImageCapture *_imgcapture; //截屏
+    bool _createmeet; // 是否创建会议
+    bool _joinmeet;   // 是否加入会议
+    bool _openCamera; // 是否打开摄像头
+
+    // MyVideoSurface *_myvideosurface;
+
+    QVideoFrame mainshow;
+
+    SendImg *_sendImg;
+    QThread *_imgThread;
+
+    RecvSolve *_recvThread;
+
+    SendText *_sendText;
+    QThread *_textThread;
+
+    MyTcpSocket *_mytcpsocket;
+//    void paintEvent(QPaintEvent *event);
+
+    //QMap
+
+    // deal message
+
+    AudioInput *_ainput;
+    QThread *_ainputThread;
+    AudioOutput *aoutput;
+
+    QStringList iplist;
+
 };
 #endif // WIDGET_H
